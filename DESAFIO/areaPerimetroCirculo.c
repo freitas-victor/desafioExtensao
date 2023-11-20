133f8fe77125149dd79af15cc38f2e508b2b79aa
freitas-victor <victorfreitas2123@gmail.com>
2023-11-20 20:16:00 -0300

    Exercício 1 54-2023

---
+++ DESAFIO/areaPerimetroCirculo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main() {
+  float area,perimetro,pi,r;
+  printf("Diga qual o raio do circulo que deseja calcular area e perimetro: \n");
+  scanf("%f",&r);
+  pi = 3.14;
+  area = pi*(r*r);
+  perimetro = 2*pi*r;
+  printf("A área deste circulo é %.2f e o perimetro é %.2f",area,perimetro);
+return 0;
+}
